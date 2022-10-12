fbe856d1823f0e36446e0c14f8fc7bf73ccc5610
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-12 12:11:22 +0200

    snack 2

---
+++ jsnack2/script.js
@@ -0,0 +1,19 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+const container = document.querySelector('.container');
+const firstWord = prompt('Inserisci una parola');
+const secondWord = prompt('Inserisci un/altra parola');
+
+const firstEl = document.createElement('div');
+const secondEl = document.createElement('div')
+firstEl.append(firstWord);
+secondEl.append(secondWord)
+
+console.log(firstWord.length);
+if (firstWord.length <= secondWord.length) {
+    container.append(firstEl);
+    container.append(secondEl);
+} else {
+    container.append(secondEl);
+    container.append(firstEl);
+}
